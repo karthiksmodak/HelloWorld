dd4252b4d3deb0d0587079e5f9d3427b11e85dd9
root <root@msi>
2024-11-27 11:58:25 +0530

    Spell-check in HelloWorld2

--- src/p1/HelloWorld2.java
+++ src/p1/HelloWorld2.java
@@ -4,6 +4,6 @@ public class HelloWorld2 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World 2 ");
-		System.out.println("Hello iniverse");
+		System.out.println("Hello Universe");
 	}
 }

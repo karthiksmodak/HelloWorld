1fa6a289e2ebfbbb6309a95f8c4f37ad65502e96
karthiksmodak <108127209+karthiksmodak@users.noreply.github.com>
2024-11-27 12:01:11 +0530

    Updated HelloWorld2.java

--- src/p1/HelloWorld2.java
+++ src/p1/HelloWorld2.java
@@ -5,5 +5,7 @@ public class HelloWorld2 {
 	public static void main(String[] args) {
 		System.out.println("Hello World 2 ");
 		System.out.println("Hello Universe");
+		System.out.println("Hello World 2 ");
+		System.out.println("Hello Universe");
 	}
 }

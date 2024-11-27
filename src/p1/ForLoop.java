73b9f225b69cf00977d89f17ad765f40d8939f79
root <root@msi>
2024-11-27 12:49:31 +0530

    Added ForLoop file

---
+++ src/p1/ForLoop.java
@@ -0,0 +1,10 @@
+package p1;
+
+public class ForLoop {
+	public static void main(String[] args) {
+		System.out.println("Demo for for loop");
+		for(int i=0;i<10;i++) {
+			System.out.println(i);
+		}
+	}
+}
